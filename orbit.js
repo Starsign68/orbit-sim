4b04573c71e78b6166658c3398232c67a8999649
kazitor <32972585+kazitor@users.noreply.github.com>
2019-04-29 22:05:25 +1000

    orbit trails

--- orbit.js
+++ orbit.js
@@ -7,91 +7,6 @@ var mouse = {
   scroll: 0
 };
 
-var system = {
-  a: 0,
-  omega: 0,
-  M: -2.78,
-  r: 6.957e8,
-  m: 2e30,
-  colour: '#fffbe0',
-  satellites: [
-    {
-      a: 5.79e10,
-      omega: 0.51,
-      M: -2.78,
-      r: 2.4e6,
-      m: 3.3e23,
-      colour: '#A7A7A3'
-    },
-    {
-      a: 1.08e11,
-      omega: 0.96,
-      M: -2.12,
-      r: 6.1e6,
-      m: 4.87e24,
-      colour: '#BEAC66'
-    },
-    {
-      a: 1.5e11,
-      omega: 1.8,
-      M: 0,
-      r: 6.4e6,
-      m: 5.97e24,
-      colour: '#2084EA',
-      satellites: [
-        {
-          a: 3.84e8,
-          omega: 1.36,
-          M: -2.6,
-          r: 1.7e6,
-          m: 7.342e22,
-          colour: '#9A9997'
-        }
-      ]
-    },
-    {
-      a: 2.28e11,
-      omega: 5.01,
-      M: -1.94,
-      r: 3.4e6,
-      m: 6.42e23,
-      colour: '#B3391D'
-    },
-    {
-      a: 7.78e11,
-      omega: 4.78,
-      M: -1.62,
-      r: 7e7,
-      m: 1.9e27,
-      colour: '#C0A180'
-    },
-    {
-      a: 1.43e12,
-      omega: 5.9,
-      M: -2.74,
-      r: 5.8e7,
-      m: 5.68e26,
-      colour: '#D1BA84'
-    },
-    {
-      a: 2.87e12,
-      omega: 1.73,
-      M: -2.35,
-      r: 2.5e7,
-      m: 8.68e25,
-      colour: '#C9EFF0'
-    },
-    {
-      a: 4.5e12,
-      omega: 4.31,
-      M: -0.54,
-      r: 2.5e7,
-      m: 1.02e26,
-      colour: '#346DF7'
-    }
-  ]
-};
-
 var GRAV = 6.674e-11;
 
 (function initialise_system(body) {
@@ -101,12 +16,15 @@ var GRAV = 6.674e-11;
   for(var i in body.satellites) {
     body.satellites[i].parent = body;
     body.satellites[i].angular_vel = Math.sqrt(GRAV*body.m/Math.pow(body.satellites[i].a, 3));
+    body.satellites[i].SOI = body.satellites[i].a * Math.pow(body.satellites[i].m / body.m, 2/5);
     initialise_system(body.satellites[i]);
   }
 })(system);
 
 var pos = new Vec(0,10000000).add(system.satellites[2].pos);
 var vel = new Vec(-36000,-8000);
+
+var pos_trail = [];
 
 var scale = 40000;
 var initial_scale = scale;
@@ -165,20 +83,27 @@ function loop(t) {
       acc.add(new Vec(mouse.x - mouse.start.x, mouse.start.y - mouse.y).scale(0.5));
     }
 
-    (function updateBodyPos(body) {
+    var dominant = system;
+
+    (function updateBody(body) {
+      var displacement = pos.to(body.pos);
+
       if(body.parent) {
         body.M += body.angular_vel * phys_step;
         if(body.M > Math.PI)
           body.M -= 2*Math.PI;
         body.pos = new Vec(body.a * Math.cos(body.M+body.omega), body.a * Math.sin(body.M+body.omega)).add(body.parent.pos);
+
+        if(dominant == body.parent && displacement.mag < body.SOI)
+          dominant = body;
       }
 
-      var displacement = pos.to(body.pos);
       acc.add(displacement.set_mag(GRAV*body.m/displacement.mag_squared));
 
       for(var i in body.satellites)
-        updateBodyPos(body.satellites[i]);
+        updateBody(body.satellites[i]);
     })(system);
+    debug_lines.dominant = dominant.name;
 
     // move/integrate
     var next_vel = new Vec(vel).add(acc.scale(phys_step));
@@ -189,7 +114,14 @@ function loop(t) {
     debug_lines.vel = vel;
 
     dt -= phys_step;
-  }
+  } // end physics
+
+  pos_trail.push({
+    parent: dominant,
+    offset: dominant.pos.to(pos)
+  });
+  if(pos_trail.length > 2048)
+    pos_trail.shift();
 
   if(mouse.scroll) {
     initial_scale = scale;
@@ -217,6 +149,21 @@ function loop(t) {
   /** render time **/
 
   ctx.drawImage(background, -300*pos.x/1e13 - 300, 300*pos.y/1e13 - 300);
+
+  ctx.strokeStyle = '#deca';
+  ctx.lineWidth = '2';
+  ctx.beginPath();
+  ctx.moveTo(
+    300 + (pos_trail[0].parent.pos.x + pos_trail[0].offset.x - pos.x)/scale,
+    300 - (pos_trail[0].parent.pos.y + pos_trail[0].offset.y - pos.y)/scale
+  );
+  for(var i = 1; i < pos_trail.length; i++) {
+    ctx.lineTo(
+      300 + (pos_trail[i].parent.pos.x + pos_trail[i].offset.x - pos.x)/scale,
+      300 - (pos_trail[i].parent.pos.y + pos_trail[i].offset.y - pos.y)/scale
+    );
+  }
+  ctx.stroke();
 
   (function drawBody(body) {
     // orbit
---
+++ system.js
@@ -0,0 +1,94 @@
+var system = {
+  name: 'Sun',
+  a: 0,
+  omega: 0,
+  M: -2.78,
+  r: 6.957e8,
+  m: 2e30,
+  colour: '#fffbe0',
+  satellites: [
+    {
+      name: 'Mercury',
+      a: 5.79e10,
+      omega: 0.51,
+      M: -2.78,
+      r: 2.4e6,
+      m: 3.3e23,
+      colour: '#A7A7A3'
+    },
+    {
+      name: 'Venus',
+      a: 1.08e11,
+      omega: 0.96,
+      M: -2.12,
+      r: 6.1e6,
+      m: 4.87e24,
+      colour: '#BEAC66'
+    },
+    {
+      name: 'Earth',
+      a: 1.5e11,
+      omega: 1.8,
+      M: 0,
+      r: 6.4e6,
+      m: 5.97e24,
+      colour: '#2084EA',
+      satellites: [
+        {
+          name: 'Moon',
+          a: 3.84e8,
+          omega: 1.36,
+          M: -2.6,
+          r: 1.7e6,
+          m: 7.342e22,
+          colour: '#9A9997'
+        }
+      ]
+    },
+    {
+      name: 'Mars',
+      a: 2.28e11,
+      omega: 5.01,
+      M: -1.94,
+      r: 3.4e6,
+      m: 6.42e23,
+      colour: '#B3391D'
+    },
+    {
+      name: 'Jupiter',
+      a: 7.78e11,
+      omega: 4.78,
+      M: -1.62,
+      r: 7e7,
+      m: 1.9e27,
+      colour: '#C0A180'
+    },
+    {
+      name: 'Saturn',
+      a: 1.43e12,
+      omega: 5.9,
+      M: -2.74,
+      r: 5.8e7,
+      m: 5.68e26,
+      colour: '#D1BA84'
+    },
+    {
+      name: 'Uranus',
+      a: 2.87e12,
+      omega: 1.73,
+      M: -2.35,
+      r: 2.5e7,
+      m: 8.68e25,
+      colour: '#C9EFF0'
+    },
+    {
+      name: 'Neptune',
+      a: 4.5e12,
+      omega: 4.31,
+      M: -0.54,
+      r: 2.5e7,
+      m: 1.02e26,
+      colour: '#346DF7'
+    }
+  ]
+};
